bd143a2eb8ea43b18bbb68f02b0dbade771b690f
zabr91 <ivanzabroda62@gmail.com>
2021-08-05 11:46:09 +0300

    first commit

---
+++ plugin.php
@@ -0,0 +1,22 @@
+/*
+Plugin Name: youtube-mass-importer-embeder
+Plugin URI: https://github.com/zabr91/youtube-mass-importer-embeder
+Description: youtube-mass-importer-embeder
+Version: 1.0.0
+Author: Ivan Zabroda
+Author URI: zabr91.github.io
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+Text Domain: youtube-mass-importer-embeder
+Domain Path: /assets/lang
+Requires PHP: 7
+*/
+//------------------------------------------------------------
+//
+// NOTE:
+//
+// Try NOT to add any code line in this file.
+//
+// Use "app\Main.php" to add your hooks.
+//
+//------------------------------------------------------------
